928ae278cf1d23db05e5c7c16a43b3232b689825
Joao Almeida <joao.almeida@hltsys.pt>
2018-09-25 09:53:55 +0100

    Update Replication

--- replication.py
+++ replication.py
@@ -70,3 +70,33 @@ def PatternMatching(Pattern, Genome):
         if Genome[i:i+len(Pattern)] == Pattern:
             positions.append(i)
     return positions
+
+
+#Count of symbol in the half genome starting at position X
+def SymbolArray(Genome, symbol):
+    array = []
+    n = len(Genome)
+    ExtendedGenome = Genome + Genome[0:n//2]
+    for i in range(n):
+        count = PatternCount(ExtendedGenome[i:i+(n//2)], symbol)
+        array.append(count)
+    return array
+
+def FasterSymbolArray(Genome, symbol):
+    array = {}
+    n = len(Genome)
+    ExtendedGenome = Genome + Genome[0:n//2]
+
+    # look at the first half of Genome to compute first array value
+    array[0] = PatternCount(symbol, Genome[0:n//2])
+
+    for i in range(1, n):
+        # start by setting the current array value equal to the previous array value
+        array[i] = array[i-1]
+
+        # the current array value can differ from the previous array value by at most 1
+        if ExtendedGenome[i-1] == symbol:
+            array[i] = array[i]-1
+        if ExtendedGenome[i+(n//2)-1] == symbol:
+            array[i] = array[i]+1
+    return array
